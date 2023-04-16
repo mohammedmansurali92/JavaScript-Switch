c05959cf1fa41f816b4d35947ea3d5d10d44e5b6
Mohammad Mansur Ali <80177222+mohammedmansurali92@users.noreply.github.com>
2023-04-17 01:42:37 +0300

    Letter grade document
    I include here the letter grade code Voel/Consonant find out.

---
+++ index.js
@@ -0,0 +1,21 @@
+var letter= prompt("Enter a letter");
+letter= letter.toLowerCase();
+switch(letter){
+   case "a":
+      document.write("Vowel");
+      break;
+      case "e":
+         document.write("Vowel");
+         break;
+         case "i":
+            document.write("Vowel");
+            break;
+            case "o":
+               document.write("Vowel");
+               break;
+               case "u":
+                  document.write("Vowel");
+                  break;
+                  default:
+                     document.write("Consonent");
+}
